7a8f5c0e6465af08686518a105fef33306d75558
Arvind Ram <arvindram03@gmail.com>
2015-11-17 04:07:45 -0500

    Adding recommendation template

--- recommendation.py
+++ recommendation.py
@@ -8,7 +8,29 @@ from flask import render_template
 import pg8000
 import sys, traceback
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
+
+usr_obj = {}
+
+data = [[[760, 801, 848, 895, 965], [733, 853, 939, 980, 1080], [714, 762, 817, 870, 918], [724, 802, 806, 871, 950]],
+    [[12, 20, 34, 10, 23], [34, 24, 12, 45, 34], [13, 46, 2, 3, 6], [33, 43, 10, 19, 18]],
+        [[67, 45, 78, 67, 68], [95, 54, 45, 68, 78], [13, 46, 2, 3, 6], [33, 43, 10, 19, 18]]]
+
+@app.route('/user')
+def users():
+  usr_obj["uid"] = request.args.get('id')
+  usr_obj["box"] = {}
+  usr_obj["box"]["data"] = data[random.randint(0, 2)]
+
+  usr_obj["box"]["outlier_data"] = [
+                    [0, 644],
+                    [4, 718],
+                    [4, 951],
+                    [4, 969]
+                  ]
+  return json.dumps(usr_obj)
+
 
 def most_popular_new_interests(user_id, max_results=10):
   user_interests = users_interests[user_key.index(user_id)]
@@ -33,7 +55,7 @@ def most_similar_users_to(user_id):
       print user_id
       conn = pg8000.connect(user="arvindram", password="", database="arvindram")
       cur = conn.cursor()
-      cur.execute("SELECT u2, sim_index FROM sim where u1 = %s",(user_id,))
+      cur.execute("SELECT u2, sim_index FROM sim_model where u1 = %s",(user_id,))
       similar_users = cur.fetchall()
       similar_users = [(user[0],float(user[1])) for user in similar_users]
       cur.close()
@@ -83,15 +105,25 @@ def similar_users():
     user_index = int(request.args.get('user'))
     return json.dumps(most_similar_users_to(user_index))
 
+@app.route('/')
+def index():
+  return app.send_static_file('index.html')
 
 @app.route('/recommendations')
 def recommendation():
+    print "HERE"
     user_index = request.args.get('user')
     suggestions = []
-    suggestions = [business_dict[s[0]] if s[0] in business_dict  else "NA" for s in most_popular_new_interests(user_index)]
-    recommendations = [business_dict[s[0]] for s in user_based_suggestions(user_index)[:20] if s[0] in business_dict and business_dict[s[0]] not in suggestions]
-    # recommendations = [business_dict[s[0]] if s[0] in business_dict else "NA" for s in user_based_suggestions(user_index)[:20]]
-    return render_template('index.html',recommendations=recommendations[:6], suggestions = suggestions[:6], user_id= user_index)        
+    recommendations = []
+    for s in most_popular_new_interests(user_index):
+      if s[0] in business_dict and business_dict[s[0]] != "":
+        suggestions.append({"title":business_dict[s[0]],"url":img_dict[s[0]]})
+
+    for s in user_based_suggestions(user_index)[:20]:
+      if s[0] in business_dict and business_dict[s[0]] != "":
+        if {"title":business_dict[s[0]],"url":img_dict[s[0]]} not in suggestions:
+          recommendations.append({"title":business_dict[s[0]],"url":img_dict[s[0]],"sim":round(s[1], 2)})
+    return render_template('recommendation.html',recommendations=recommendations[:6], suggestions = suggestions[:6], user_id= user_index)        
 
 if __name__ == '__main__':
   
@@ -101,7 +133,6 @@ if __name__ == '__main__':
   line = f.readline()
   line = f.readline()
   
-  print "Reading movie rewiew..."
   while line:
     parts = line.split(",")
     user_id = parts[0]
@@ -117,15 +148,20 @@ if __name__ == '__main__':
   print "Reading movie titles..."
   global business_dict
   business_dict = {}
+  global img_dict
+  img_dict = {}
   f = open("/Users/arvindram/Documents/DataScience/final_project/movies-meta-py.csv", "r")
   line = f.readline()
   line = f.readline()
 
   while line:
     parts = line.split(",")
+    
     business_id = parts[0]
-    name = parts[1].replace('\n','').replace('"','')
+    name = parts[1].replace('"','')
+    url = parts[2].replace('\n','')
     business_dict[business_id] = name
+    img_dict[business_id] = url
     line = f.readline()  
 
   f.close()
